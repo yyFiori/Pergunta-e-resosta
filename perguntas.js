66b63db7a75dbc3f0c9a1e8a3590302ae102b428
yyFiori <oiterooitero2@gmail.com>
2024-10-15 08:33:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'O que é preterito perfeito?',
+    'Pretérito perfeito é um tempo gramatical ou modo verbal que serve para denotar eventos que ocorreram ou foram concluídos no passado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz 20 em ingles?',
+    '20 em ingles é twenty'
+)
